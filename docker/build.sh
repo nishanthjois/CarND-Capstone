91997be6c234be5ac35878f708435691dd89a0b4
Carlos Galvez <carlosgalvezp@gmail.com>
2017-08-29 15:40:19 +0200

    Tag the Docker image when building

--- docker/build.sh
+++ docker/build.sh
@@ -2,4 +2,6 @@
 set -e
 
 THIS_DIR="$(readlink -m "$(dirname "$0")")"
-docker build -f "$THIS_DIR/Dockerfile" "$THIS_DIR/.."
+docker build --file "$THIS_DIR/Dockerfile"             \
+             --tag carlosgalvezp/carnd_capstone:latest \
+             "$THIS_DIR/.."
