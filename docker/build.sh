42e0f68acc1efec1160b2450ff0e58d0bfad44dd
Oren <oren@Orens-MacBook-Pro-2.local>
2017-09-07 08:26:36 +0300

    small scripts update

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-THIS_DIR="$(readlink -m "$(dirname "$0")")"
+THIS_DIR="$(cd "$(dirname "$0")" && pwd -P && cd - > /dev/null)"
+
 docker build --file "$THIS_DIR/Dockerfile"             \
              --tag eurobots/carnd_capstone:latest      \
              "$THIS_DIR/.."
