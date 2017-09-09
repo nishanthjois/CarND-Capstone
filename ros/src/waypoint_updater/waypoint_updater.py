ed96a6d8ef2de68a8a61b949135fe0ceaae86e77
Ivan Danov <idanov@users.noreply.github.com>
2017-09-09 20:49:46 +0300

    Implement the partial waypoint updater (#29)
    * Subscribe to /traffic_waypoint and /obstacle_waypoint in Waypoint Updater
    
    * Keep the waypoints and current pose
    
    * Convert quaternion to yaw for detecting the next waypoint
    
    * Find next waypoint from current pose
    
    * Publish preliminary /final_waypoints data

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
-import rospy
-from geometry_msgs.msg import PoseStamped
-from styx_msgs.msg import Lane, Waypoint
-
+import copy
 import math
+
+import rospy
+import tf
+from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import Int32
+from styx_msgs.msg import Lane
 
 '''
 This node will publish waypoints from the car's current position
@@ -33,23 +36,41 @@ class WaypointUpdater(object):
 
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
+        rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
+        rospy.Subscriber('/obstacle_waypoint', Lane, self.obstacle_cb)
 
-        # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint
-
-        self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane,
-                                                   queue_size=1)
+        self.pub = rospy.Publisher('/final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
+        self.waypoints = None
+        self.ego = None
 
         rospy.spin()
 
-    def pose_cb(self, msg):
-        # TODO: Implement
-        pass
+    def publish(self):
+        idx = self.find_next_waypoint()
+        if idx > -1 and not rospy.is_shutdown():
+            rospy.loginfo("Current position ({}, {}), next waypoint: {}"
+                          .format(self.ego.pose.position.x,
+                                  self.ego.pose.position.y,
+                                  idx))
+            waypoints = self.waypoints + self.waypoints
+            waypoints = waypoints[idx:idx+LOOKAHEAD_WPS]
+            lane = Lane()
+            lane.header.frame_id = '/world'
+            lane.header.stamp = rospy.Time.now()
+            lane.waypoints = waypoints
+            self.pub.publish(lane)
 
-    def waypoints_cb(self, waypoints):
-        # TODO: Implement
-        pass
+    def pose_cb(self, msg):
+        if self.ego is None or self.ego.header.seq < msg.header.seq:
+            # TODO: Calculate ego's velocity (and maybe acceleration) if needed
+            self.ego = msg
+            self.publish()
+
+    def waypoints_cb(self, lane):
+        if self.waypoints is None:
+            self.waypoints = lane.waypoints
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
@@ -60,22 +81,64 @@ class WaypointUpdater(object):
         # We will implement it later
         pass
 
-    def get_waypoint_velocity(self, waypoint):
+    @classmethod
+    def yaw_from_quaternion(cls, q):
+        quaternion = [q.x, q.y, q.z, q.w]
+        _, _, yaw = tf.transformations.euler_from_quaternion(quaternion)
+        return yaw
+
+    def find_next_waypoint(self):
+        min_dist = 1e10
+        min_idx = -1
+        if self.ego and self.waypoints:
+            ego_pose = self.ego.pose
+            n_waypoints = len(self.waypoints)
+            # Find the closest waypoint
+            for i in range(n_waypoints):
+                wp_pos = self.waypoints[i].pose.pose.position
+                dl = self.euclidean(ego_pose.position, wp_pos)
+                if dl < min_dist:
+                    min_dist = dl
+                    min_idx = i
+
+            # Check if we are behind or past the closest waypoint
+            wp_pos = self.waypoints[min_idx].pose.pose.position
+            pos = copy.deepcopy(ego_pose.position)
+            yaw = self.yaw_from_quaternion(ego_pose.orientation)
+            pos.x += math.cos(yaw) * .1
+            pos.y += math.sin(yaw) * .1
+            if self.euclidean(wp_pos, pos) > min_dist:
+                min_idx = (min_idx + 1) % n_waypoints
+        return min_idx
+
+    @staticmethod
+    def get_waypoint_velocity(waypoint):
         return waypoint.twist.twist.linear.x
 
-    def set_waypoint_velocity(self, waypoints, waypoint, velocity):
+    @staticmethod
+    def set_waypoint_velocity(waypoints, waypoint, velocity):
         waypoints[waypoint].twist.twist.linear.x = velocity
 
-    def distance(self, waypoints, wp1, wp2):
-        def dl(a, b):
-            return math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2 + (a.z-b.z)**2)
+    @classmethod
+    def euclidean(cls, pos1, pos2):
+        """
+        Return the Euclidean distance between two points
 
-        dist = 0
+        :param pos1: geometry_msgs/Point
+        :param pos2: geometry_msgs/Point
+        :return: Euclidean distance between two points
+        """
+        return math.sqrt((pos1.x - pos2.x) ** 2 + (pos1.y - pos2.y) ** 2 +
+                         (pos1.z - pos2.z) ** 2)
+
+    @classmethod
+    def distance(cls, waypoints, wp1, wp2):
+        dl = 0
         for i in range(wp1, wp2+1):
-            dist += dl(waypoints[wp1].pose.pose.position,
-                       waypoints[i].pose.pose.position)
+            dl += cls.euclidean(waypoints[wp1].pose.pose.position,
+                                waypoints[i].pose.pose.position)
             wp1 = i
-        return dist
+        return dl
 
 
 if __name__ == '__main__':
